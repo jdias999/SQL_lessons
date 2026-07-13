b0e735b16b7fdbf272b8f542385a6d60159d5fff
Joao Dias <jd9198825@gmail.com>
2026-07-13 15:29:17 -0300

    Final de nested functions

--- Functions/Nested_functions.sql
+++ Functions/Nested_functions.sql
@@ -28,7 +28,7 @@ SELECT
     S.type
 FROM skill_struct;
 
-----------------------------------------------------------------------------------------------
+--Outra struct
 
 WITH skill_table AS (
     SELECT 'python' AS skills, 'programming' AS types
@@ -44,4 +44,35 @@ SELECT
     ) 
 FROM skill_table;
 
---
+
+--Exemplo de json
+
+
+WITH raw_skill_json AS (
+    SELECT
+        '{"skill":"python", "type":"programming"}'::JSON AS skill_json
+)
+SELECT
+    STRUCT_PACK (
+        skill := json_extract_string(skill_json, '$.skill'),
+        types := json_extract_string(skill_json, '$.type')
+    )
+FROM
+    raw_skill_json;
+
+--Exemplo transformando skills de uma unica vaga, em uma list
+
+SELECT
+    jpf.job_id,
+    jpf.job_title_short,
+    jpf.salary_year_avg,
+    ARRAY_AGG(sd.skills) AS skills_array
+FROM
+    job_postings_fact AS jpf
+LEFT JOIN
+    skills_job_dim AS sjd 
+ON jpf.job_id = sjd.job_id
+LEFT JOIN
+    skills_dim AS sd
+ON sd.skill_id = sjd.skill_id
+GROUP BY ALL;
